4911f3fa4b339d702a3407b194bf1c3867fa6480
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2024-06-11 05:56:57 +0600

    added self link to the nav logo
    redirects to the dashboard page

--- components/Header/Logo.tsx
+++ components/Header/Logo.tsx
@@ -1,14 +1,16 @@
 export default function Logo() {
   return (
-    <div className="w-[max-content] flex gap-2 items-center">
-      <img
-        src="/logo.png"
-        alt="Logo"
-        width={48}
-        height={48}
-        className="w-[24px] h-[24px] md:w-[48px] md:h-[48px]"
-      />
-      <h1 className="text-xl md:text-2xl">Eventsync</h1>
-    </div>
+    <a href="/dashboard">
+      <div className="w-[max-content] flex gap-2 items-center">
+        <img
+          src="/logo.png"
+          alt="Logo"
+          width={48}
+          height={48}
+          className="w-[24px] h-[24px] md:w-[48px] md:h-[48px]"
+        />
+        <h1 className="text-xl md:text-2xl">Eventsync</h1>
+      </div>
+    </a>
   );
 }
