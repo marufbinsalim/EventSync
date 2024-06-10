20d1bcb5ea3bfec92157ee07c8a88b2404d6a76f
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2024-06-10 22:19:46 +0600

    moved the page protection logic to the top of the V-DOM

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,36 @@
 import "@/styles/globals.css";
+import { supabase } from "@/utils/supabase/client";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    let nextRoute = "/";
+    const authState = supabase.auth.onAuthStateChange((event, session) => {
+      switch (event) {
+        case "INITIAL_SESSION":
+          nextRoute = session ? "/dashboard" : "/";
+          break;
+        case "SIGNED_IN":
+          nextRoute = "/dashboard";
+          break;
+        case "SIGNED_OUT":
+          nextRoute = "/";
+          break;
+        case "TOKEN_REFRESHED":
+          nextRoute = session ? "/dashboard" : "/";
+          break;
+      }
+      router.push(nextRoute);
+    });
+
+    return () => {
+      authState.data.subscription.unsubscribe();
+    };
+  }, []);
+
   return <Component {...pageProps} />;
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,35 +1,7 @@
 import AuthUI from "@/components/AuthUI";
-import { supabase } from "@/utils/supabase/client";
-import { Auth } from "@supabase/auth-ui-react";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
-import { Star, StarHalfIcon } from "lucide-react";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { Star } from "lucide-react";
 
 export default function AuthenticationScreen() {
-  const [session, setSession] = useState(null);
-  const router = useRouter();
-
-  useEffect(() => {
-    const data = supabase.auth.onAuthStateChange((event, session) => {
-      if (event === "INITIAL_SESSION" && session) {
-        router.push("/dashboard");
-      } else {
-        setSession(session);
-      }
-    });
-
-    return () => {
-      data.data.subscription.unsubscribe();
-    };
-  }, []);
-
-  useEffect(() => {
-    if (session) {
-      router.push("/dashboard");
-    }
-  }, [session, router]);
-
   return (
     <div className="flex flex-col items-center h-screen bg-slate-900 text-white">
       <div className="bg-red-300 w-full flex justify-center p-4">
