b25122fc8c360aee7d7eee9c67bcfd43431ff938
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2024-06-11 03:47:58 +0600

    Loading spinner in AuthScreen
    --  added a loading spinner when auth is loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
 import AuthUI from "@/components/AuthUI";
 import Logo from "@/components/Logo";
 import { RootState } from "@/state/store";
+import { CircleDashed } from "lucide-react";
 import { useSelector } from "react-redux";
 
 export default function AuthScreen() {
   const authState = useSelector((state: RootState) => state.auth);
-  console.log(authState.session);
 
   if (authState.isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="flex items-center justify-center h-screen text-white bg-slate-900">
+        <CircleDashed className="w-12 h-12 animate-spin" />
+      </div>
+    );
   }
 
   return (
