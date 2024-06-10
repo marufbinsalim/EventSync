e7eaaa581fbf51c2816e5d681558910ce4c49738
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2024-06-10 22:53:17 +0600

    created a hook for getting the auth state and protecting routes

---
+++ hooks/useAuthSubscription.ts
@@ -0,0 +1,63 @@
+import { supabase } from "@/utils/supabase/client";
+import type { AuthChangeEvent, Session } from "@supabase/supabase-js";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+export default function useAuthSubscription() {
+  const router = useRouter();
+  const [nextRoute, setNextRoute] = useState("");
+  const [session, setSession] = useState<Session | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  function handleEvent(
+    event: AuthChangeEvent,
+    session: Session | null
+  ): { nextRoute: string } {
+    let next = "";
+    switch (event) {
+      case "INITIAL_SESSION":
+        next = session ? "/dashboard" : "/";
+        break;
+      case "TOKEN_REFRESHED":
+        next = session ? "/dashboard" : "/";
+        break;
+      case "SIGNED_IN":
+        next = "/dashboard";
+        break;
+      case "SIGNED_OUT":
+        next = "/";
+        break;
+    }
+
+    return {
+      nextRoute: next,
+    };
+  }
+
+  useEffect(() => {
+    setIsLoading(true);
+    const authState = supabase.auth.onAuthStateChange((event, session) => {
+      const { nextRoute } = handleEvent(event, session);
+      setNextRoute(nextRoute);
+      setSession(session);
+      setIsLoading(false);
+    });
+
+    return () => {
+      authState.data.subscription.unsubscribe();
+      setIsLoading(false);
+    };
+  }, []);
+
+  useEffect(() => {
+    // Redirect to the next route if it's not the current route
+    if (nextRoute !== "" && router.pathname !== nextRoute) {
+      router.push(nextRoute);
+    }
+  }, [nextRoute, router]);
+
+  return {
+    session,
+    isAuthStateLoading: isLoading,
+  };
+}
