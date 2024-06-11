2c72cc718650ae17d024c449d91673baa8d16bc3
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2024-06-11 15:28:43 +0600

    added input label and additional styles

--- components/AddressInput/AddressInput.tsx
+++ components/AddressInput/AddressInput.tsx
@@ -2,14 +2,20 @@ import useDebouncedAddressSuggestion from "@/hooks/useDebouncedAddressSuggestion
 import { CircleDashed, SearchCheck, XCircleIcon } from "lucide-react";
 import { useState } from "react";
 
-export default function AddressInput() {
+export default function AddressInput({
+  selectedAddress,
+  setSelectedAddress,
+}: {
+  selectedAddress: string;
+  setSelectedAddress: (address: string) => void;
+}) {
   const [address, setAddress] = useState<string>("");
-  const [selectedAddress, setSelectedAddress] = useState<string>("");
+
   const { suggestions, setSuggestions, loading, setLoading } =
     useDebouncedAddressSuggestion(address, selectedAddress);
 
   return (
-    <div className="relative w-[100%] md:w-[max-content] p-2 bg-slate-800 flex justify-center">
+    <div className="relative w-[100%] md:w-[max-content] bg-slate-800 flex justify-center">
       <div className="flex items-center gap-2">
         <input
           type="text"
@@ -41,30 +47,32 @@ export default function AddressInput() {
         </button>
       </div>
 
-      <div className="absolute top-[100%] left-0 w-full bg-white rounded-md shadow-lg">
-        {loading && (
-          <div className="flex justify-center w-full p-2 border-b border-gray-200 cursor-pointer last:border-0 hover:bg-slate-800 bg-slate-700 text-slate-200">
-            <CircleDashed className="w-6 h-6 animate-spin" />
-          </div>
-        )}
-
-        {suggestions &&
-          suggestions.length > 0 &&
-          suggestions.map((suggestion, index) => (
-            <div
-              key={index}
-              className="p-2 border-b cursor-pointer border-slate-500 hover:bg-slate-800 bg-slate-700 text-slate-200"
-              onClick={() => {
-                setAddress(suggestion.display_name);
-                setSelectedAddress(suggestion.display_name);
-                setSuggestions([]);
-                setLoading(false);
-              }}
-            >
-              {suggestion.display_name}
+      {(loading || suggestions.length > 0) && (
+        <div className="absolute top-[120%] left-0 w-[100%] bg-slate-700 rounded-md shadow-lg p-2">
+          {loading && (
+            <div className="flex justify-center w-full p-2 border-b cursor-pointer border-sate-500 last:border-0 hover:bg-slate-800 bg-slate-700 text-slate-200">
+              <CircleDashed className="w-6 h-6 animate-spin" />
             </div>
-          ))}
-      </div>
+          )}
+
+          {suggestions &&
+            suggestions.length > 0 &&
+            suggestions.map((suggestion, index) => (
+              <div
+                key={index}
+                className="p-2 border-b cursor-pointer border-slate-500 hover:bg-slate-800 bg-slate-700 text-slate-200"
+                onClick={() => {
+                  setAddress(suggestion.display_name);
+                  setSelectedAddress(suggestion.display_name);
+                  setSuggestions([]);
+                  setLoading(false);
+                }}
+              >
+                {suggestion.display_name}
+              </div>
+            ))}
+        </div>
+      )}
     </div>
   );
 }
--- pages/add-event.tsx
+++ pages/add-event.tsx
@@ -2,8 +2,10 @@ import AddressInput from "@/components/AddressInput/AddressInput";
 import Header from "@/components/Header/Header";
 import NavigationBar from "@/components/Nav/NavigationBar";
 import Head from "next/head";
+import { useState } from "react";
 
 export default function Add() {
+  const [selectedAddress, setSelectedAddress] = useState<string>("");
   return (
     <>
       <Head>
@@ -13,8 +15,14 @@ export default function Add() {
       <div className="flex flex-col h-[100%] overflow-y-hidden">
         <Header />
         <NavigationBar />
-        <div className="flex-1 w-full overflow-y-scroll styled-scroll">
-          <AddressInput />
+        <div className="flex-1 w-full overflow-y-scroll styled-scroll bg-slate-800">
+          <div className="flex flex-wrap items-center gap-2 px-4 py-2">
+            <p className="text-lg text-white">Address</p>
+            <AddressInput
+              selectedAddress={selectedAddress}
+              setSelectedAddress={setSelectedAddress}
+            />
+          </div>
         </div>
         <div>Footer</div>
       </div>
