87ce97f2a6a046c7fd6e86e26e00f6cb31b561d0
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2024-06-11 06:47:25 +0600

    added base layout style for profile and dashboard
    -- has main content that's scrollable
    -- nav and footer always visible
    -- used flex to achieve the styling
    -- scrollbar is custom styled

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header/Header";
 import NavigationBar from "@/components/Nav/NavigationBar";
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Dashboard() {
   return (
@@ -9,9 +10,20 @@ export default function Dashboard() {
         <title>Dashboard @ Eventsync</title>
         <meta name="description" content="Dashboard" />
       </Head>
-      <div>
+      <div className="flex flex-col h-[100%] overflow-y-hidden">
         <Header />
         <NavigationBar />
+        <div className="flex-1 overflow-y-scroll styled-scroll">
+          {
+            // 1000 divs rendered
+            Array.from({ length: 1000 }, (_, i) => (
+              <div key={i} className="p-4 bg-slate-800">
+                <p className="text-white">Content {i + 1}</p>
+              </div>
+            ))
+          }
+        </div>
+        <div>Footer</div>
       </div>
     </>
   );
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -9,9 +9,20 @@ export default function Profile() {
         <title>Profile @ Eventsync</title>
         <meta name="description" content="profile page!" />
       </Head>
-      <div>
+      <div className="flex flex-col h-[100%] overflow-y-hidden">
         <Header />
         <NavigationBar />
+        <div className="flex-1 overflow-y-scroll styled-scroll">
+          {
+            // 1000 divs rendered
+            Array.from({ length: 1000 }, (_, i) => (
+              <div key={i} className="p-4 bg-slate-800">
+                <p className="text-white">Content {i + 1}</p>
+              </div>
+            ))
+          }
+        </div>
+        <div>Footer</div>
       </div>
     </>
   );
